65014b5b52b85d4f4ec413b62129f81914b7c45b
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2026-04-06 20:09:21 +0700

    tambah DTO kategori

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ProductsModule],
+  imports: [PrismaModule, AuthModule, ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
