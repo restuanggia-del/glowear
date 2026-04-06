53f239548d8d2d257d22315f699cb21fa43ddfae
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2026-04-06 20:10:07 +0700

    buat validasi dto kategori

--- backend/src/categories/dto/create-category.dto.ts
+++ backend/src/categories/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Nama kategori tidak boleh kosong' })
+  namaKategori!: string;
+
+  @IsString()
+  @IsOptional()
+  deskripsi?: string;
+}
