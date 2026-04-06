2010d7f4932b2ce28e54ef49a6e0261bdee3828d
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2026-04-06 19:58:02 +0700

    buat dto untuk validasi data ke frontend

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,1 +1,28 @@
-export class CreateProductDto {}
+import { IsString, IsInt, IsOptional, IsNotEmpty, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Nama produk tidak boleh kosong' })
+  namaProduk!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Deskripsi tidak boleh kosong' })
+  deskripsi!: string;
+
+  @IsInt()
+  @Min(0, { message: 'Harga tidak boleh minus' })
+  harga!: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  stok?: number;
+
+  @IsString()
+  @IsOptional()
+  gambar?: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'ID Kategori tidak boleh kosong' })
+  categoryId!: string;
+}
