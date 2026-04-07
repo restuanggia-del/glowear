2a2316285c7b89b372f91361672ed3bb52a7cc11
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-04-07 09:14:57 +0700

    membuat link router (klik Login) app/register.tsx ke halaman app/login.tsx

--- mobile-app/app/register.tsx
+++ mobile-app/app/register.tsx
@@ -7,6 +7,7 @@ import {
   StyleSheet,
 } from "react-native";
 import { api } from "./services/api";
+import { router } from "expo-router";
 
 export default function Register() {
   const [nama, setNama] = useState("");
@@ -75,7 +76,10 @@ export default function Register() {
       </View>
 
       <Text style={styles.footer}>
-        Sudah punya akun? <Text style={styles.link}>Login</Text>
+        Sudah punya akun?{" "}
+        <Text style={styles.link} onPress={() => router.push("/login")}>
+          Login
+        </Text>
       </Text>
     </View>
   );
