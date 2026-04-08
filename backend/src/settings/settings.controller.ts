9da8f4c7db68580e53158b73ccab59761d82fa5a
restuanggia-del <restuanggia10@gmail.com>
2026-04-08 11:26:17 +0700

    delete findOne, update, atau remove yang tersisa

--- backend/src/settings/settings.controller.ts
+++ backend/src/settings/settings.controller.ts
@@ -1,39 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Put, Body } from '@nestjs/common';
 import { SettingsService } from './settings.service';
-import { CreateSettingDto } from './dto/create-setting.dto';
-import { UpdateSettingDto } from './dto/update-setting.dto';
 
 @Controller('settings')
 export class SettingsController {
   constructor(private readonly settingsService: SettingsService) {}
 
-  @Post()
-  create(@Body() createSettingDto: CreateSettingDto) {
-    return this.settingsService.create(createSettingDto);
-  }
-
+  // Hanya ada 2 fungsi ini, pastikan tidak ada fungsi bawaan NestJS yang tersisa
   @Get()
-  findAll() {
-    return this.settingsService.findAll();
+  getSettings() {
+    return this.settingsService.getSettings();
   }
 
   @Put()
   updateSettings(@Body() updateData: any) {
     return this.settingsService.updateSettings(updateData);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.settingsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSettingDto: UpdateSettingDto) {
-    return this.settingsService.update(+id, updateSettingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.settingsService.remove(+id);
   }
 }
