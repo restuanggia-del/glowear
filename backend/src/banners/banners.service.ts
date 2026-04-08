6b4739554d31bbe661b9351080bb695b89e20e75
restuanggia-del <restuanggia10@gmail.com>
2026-04-08 11:46:06 +0700

    tambahkan service agar bisa endpoint di model banner

--- backend/src/banners/banners.service.ts
+++ backend/src/banners/banners.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBannerDto } from './dto/create-banner.dto';
 import { UpdateBannerDto } from './dto/update-banner.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class BannersService {
+  constructor(private prisma: PrismaService) {}
   create(createBannerDto: CreateBannerDto) {
     return 'This action adds a new banner';
   }
 
   findAll() {
-    return `This action returns all banners`;
+    return this.prisma.banner.findMany({ orderBy: { waktuDibuat: 'desc' } });
   }
 
   findOne(id: number) {
