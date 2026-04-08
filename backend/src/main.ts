42f7208f02004355c75aa9ce7a45076e8285bb4f
restuanggia-del <restuanggia10@gmail.com>
2026-04-08 11:37:51 +0700

    tambahkan instate app agar folder uplouds bisa di akses forntend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   // 1. Buat instance 'app' terlebih dahulu
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // 2. Terapkan global pipes setelah 'app' terbuat
   app.useGlobalPipes(new ValidationPipe({ 
@@ -17,6 +19,10 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
   });
 
   // 4. Jalankan server
