a6574e1445d254306c7b8b1e8150ddb9e4d920c6
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2026-04-09 15:51:08 +0700

    ubah port baseURL 3000 menjadi 3001

--- mobile-app/app/services/api.ts
+++ mobile-app/app/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.18.177:3000", // ganti IP kamu
+  baseURL: "http://192.168.18.177:3001", // ganti IP kamu
 });
