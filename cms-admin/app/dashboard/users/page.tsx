cfb0af19a7647114fdeffdd6ce36e84e37158539
faiz140405 <chamargablok51@gmail.com>
2026-04-16 08:49:20 +0700

    ubah method endpoint yang awalanya PUT menjadi PATCH

--- cms-admin/app/dashboard/users/page.tsx
+++ cms-admin/app/dashboard/users/page.tsx
@@ -90,7 +90,7 @@ export default function UsersPage() {
 
     try {
       const res = await fetch(`http://localhost:3001/users/${selectedUser.id}`, {
-        method: "PUT",
+        method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
       });
